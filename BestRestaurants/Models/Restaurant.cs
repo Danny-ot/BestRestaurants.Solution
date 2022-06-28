ab500e70035a9bed8357930a21d9a1fea7620ecc
Danny-ot <oladejidaniel456@gmail.com>
2022-06-28 02:38:07 -0700

    Create the model backend for the Restaurant

---
+++ BestRestaurants/Models/Restaurant.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+namespace BestRestaurants.Models
+{
+    public class Restaurant
+    {
+        public Restaurant()
+        {
+            this.Reviews = new HashSet<Review>();
+        }
+        public int RestaurantId{get; set;}
+        public string Name{get; set;}
+        public string Address{get; set;}
+        public string Description{get; set;}
+        public string ServiceOffered{get; set;}
+        public string PaymentForm{get; set;}
+        public string Country{get; set;}
+        public int CuisineId{get; set;}
+        public virtual Cuisine Cuisine{get; set;}
+        public virtual ICollection<Review> Reviews{get; set;} 
+    }
+}
