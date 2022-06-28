7b681c8e00db85538316336ee250824c8f84590f
Danny-ot <oladejidaniel456@gmail.com>
2022-06-28 02:36:40 -0700

    Create the DBContext and connect the tables

---
+++ BestRestaurants/Models/BestRestaurantsContext.cs
@@ -0,0 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace BestRestaurants.Models
+{
+    public class BestRestaurantsContext : DbContext
+    {
+        public DbSet<Cuisine> Cuisines{get; set;}
+        public DbSet<Restaurant> Restaurants{get; set;}
+
+        public DbSet<Review> Reviews{get; set;}
+
+        public BestRestaurantsContext(DbContextOptions options) : base(options)
+        {    
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseLazyLoadingProxies();
+        }
+    }
+}
